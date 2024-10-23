fedc5609134d92adcf14cf5a6aea3fedac83e3e8
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-10-23 16:35:42 -0300

    subindo gambiarras que fiz no compoente search para fazer ele conseguir pegar objetos dentro de objetos kkkkkkkkkk

--- src/app/pages/OS/OS.component.ts
+++ src/app/pages/OS/OS.component.ts
@@ -5,11 +5,17 @@ import { ElectronService } from '../../shared/services/electron.service';
 import { OSTable } from '../../shared/interfaces/os-table.interface';
 import { ScreenshotComponent } from '../../shared/components/screenshot/screenshot.component';
 import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchComponent } from '../../shared/components/search/search.component';
 
 @Component({
   selector: 'app-os',
   standalone: true,
-  imports: [TableDataComponent, ScreenshotComponent, NgbModalModule],
+  imports: [
+    TableDataComponent,
+    ScreenshotComponent,
+    NgbModalModule,
+    SearchComponent,
+  ],
   templateUrl: './os.component.html',
   styleUrl: './os.component.scss',
 })
@@ -23,25 +29,24 @@ export class OSComponent implements OnInit {
     this.loadData();
   }
 
-  async loadData(os?: any) {
+  async loadData(os?: any[]) {
+    this.listaOs = [];
     if (os) {
-      this.listaOs = os;
+      this.os = os;
     } else {
-      this.listaOs = [];
-
       this.os = await this.http.loadData('os');
-      if (this.os.length > 0) {
-        for (let item of this.os) {
-          const obj: OSTable = {
-            id: item.id,
-            nomeFirma: item.customer.name,
-            emissao: item.dataEmissao,
-            carro: item.car.modelo,
-            fone: item.customer.telephone,
-            descricao: item.descricaoServico,
-          };
-          this.listaOs.push(obj);
-        }
+    }
+    if (this.os.length > 0) {
+      for (let item of this.os) {
+        const obj: OSTable = {
+          id: item.id,
+          nomeFirma: item.customer.name,
+          emissao: item.dataEmissao,
+          carro: item.car.modelo,
+          fone: item.customer.telephone,
+          descricao: item.descricaoServico,
+        };
+        this.listaOs.push(obj);
       }
     }
   }
--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -8,39 +8,49 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss',
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
-  @Output() emitterData: EventEmitter<any> = new EventEmitter<any>();
-  @Input() table = '';
+  @Output() emitterData: EventEmitter<any[]> = new EventEmitter<any[]>();
+  @Input() table: string = '';
 
-  public searchTerm = '';
+  public searchTerm: string = '';
   public filteredItems: any[] = [];
 
   constructor(private http: ElectronService) {}
 
   async loadData() {
+    this.filteredItems = [];
+    const res: any[] = await this.http.loadData(this.table);
+
     if (!this.searchTerm) {
-      this.filteredItems = [];
-      const res: any[] = await this.http.loadData(this.table);
       this.filteredItems = res;
     } else {
-      this.filteredItems = [];
-      const res: any[] = await this.http.loadData(this.table);
       for (let obj of res) {
-        for (let key in obj) {
-          if (
-            obj[key]
-              .toString()
-              .toLowerCase()
-              .includes(this.searchTerm.toLowerCase())
-          ) {
-            this.filteredItems.push(obj);
-            break;
-          }
+        if (this.searchInObject(obj)) {
+          this.filteredItems.push(obj);
         }
       }
     }
+
     this.emitterData.emit(this.filteredItems);
+  }
+
+  private searchInObject(obj: any[]): boolean {
+    for (let key in obj) {
+      if (typeof obj[key] === 'object') {
+        if (this.searchInObject(obj[key])) {
+          return true;
+        }
+      } else if (
+        obj[key]
+          .toString()
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase())
+      ) {
+        return true;
+      }
+    }
+    return false;
   }
 }
